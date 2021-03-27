707b1c75339e3e807746c234f57bf35bdb119a05
fredrik fusdahl <fredrikfusdahl@fredriks-MBP.home>
2021-03-27 19:09:30 +0100

    update

--- FFVolModel.py
+++ FFVolModel.py
@@ -12,10 +12,8 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
-
-
-
-
+from functions import *
+os.chdir("/Users/fredrikfusdahl/Documents/MasterThesis")
 #Import CRSP data
 data = pd.read_csv('testData.csv')
 data = data.dropna()
@@ -23,7 +21,54 @@ data = data.dropna()
 #Get unique set with company permno
 permno = data.PERMNO.unique()
 
-#%% 
-#We will first run the 
 data['CLOSEPRC'] = (data['ASK'] + data['BID'])/2
 data['SIZE'] = data['SHROUT']*data['CLOSEPRC']
+
+#Create measure for daily, weekly and monthly vol. 
+df = RV(data, [5, 20])
+
+#Estimate model 
+
+
+#%%
+start_of_outofsample = 20190000
+unique_date = df['date'].loc[df['date'] > 20190000].unique()
+
+
+#Estimate model and measure insample fit for 500 most liquid stocks in 2020
+df_2020 = df.loc[df['date'] < 20200000]
+#Unique stocks 
+permno_2020 = df_2020.PERMNO.unique()
+
+mean_vol = np.zeros((permno_2020.shape[0], 1))
+for i in range(0, permno_2020.shape[0]):
+    mean_vol[i] = df_2020['VOL'].loc[df_2020['PERMNO'] == permno_2020[i]].mean()
+
+#choose the 500 PERMNO codes with highest average volume
+permno_2020 = 
+    
+
+
+
+    
+    
+
+
+
+
+    
+
+#%%    
+    
+    
+
+ 
+                
+        
+    
+    
+
+
+
+
+
--- functions.py
+++ functions.py
@@ -1,3 +1,24 @@
-def test_function():
-	return 'halla'
+import pandas as pd 
+import numpy as np
+
+
+
+def RV(df, typeVOL = None): 
+    #First calculate daily Volatility
+    df['dVOL'] = np.log(df['ASKHI']) - np.log(df['BIDLO'])
+    if(typeVOL != None):
+        if(len(typeVOL)>0): 
+            for typ in typeVOL:
+                df[str(typ)+'VOL'] = (df['dVOL'].rolling(typ).sum())/typ    
+        else:
+            df[str(typeVOL)+'VOL'] = (df['dVOL'].rolling(typeVOL).sum())/typeVOL
+    
+    return(df)
+
+
+def sorted_enumerate(seq):
+    return [i for (v, i) in sorted((v, i) for (i, v) in enumerate(seq))]
+
+
+
 
