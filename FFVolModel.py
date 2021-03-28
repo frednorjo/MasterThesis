098ef4acde2b34001a71c5bfe3edb60ea050a096
fredrik fusdahl <fredrikfusdahl@fredriks-MBP.home>
2021-03-28 14:10:18 +0200

    new update

--- FFVolModel.py
+++ FFVolModel.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
 from functions import *
+from sklearn.linear_model import LinearRegression
 os.chdir("/Users/fredrikfusdahl/Documents/MasterThesis")
 #Import CRSP data
 data = pd.read_csv('testData.csv')
@@ -27,30 +28,36 @@ data['SIZE'] = data['SHROUT']*data['CLOSEPRC']
 #Create measure for daily, weekly and monthly vol. 
 df = RV(data, [5, 20])
 
-#Estimate model 
+#Estimate model HAR model in panel data
+df['future'] = df.groupby('PERMNO')['dVOL'].shift(-1)
+df = df.dropna()
 
+#%%# Estimate model HAR model in panel data
+X = df[['dVOL', '5VOL', '20VOL']]
+y = df['future']
+lm = LinearRegression().fit(X,y)
 
 #%%
-start_of_outofsample = 20190000
-unique_date = df['date'].loc[df['date'] > 20190000].unique()
-
-
 #Estimate model and measure insample fit for 500 most liquid stocks in 2020
-df_2020 = df.loc[df['date'] < 20200000]
-#Unique stocks 
-permno_2020 = df_2020.PERMNO.unique()
-
-mean_vol = np.zeros((permno_2020.shape[0], 1))
-for i in range(0, permno_2020.shape[0]):
-    mean_vol[i] = df_2020['VOL'].loc[df_2020['PERMNO'] == permno_2020[i]].mean()
+df_2020 = df.loc[df['date'] > 20200000]
+mean_volume = df_2020.groupby('PERMNO')['VOL'].mean()
 
 #choose the 500 PERMNO codes with highest average volume
-permno_2020 = 
+ind = sorted_enumerate(mean_volume)
+permno_2020 = permno_2020[ind[-500:-1]]
+
+for i in range(start = 0, stop = permno_2020.shape[0]):
+    
+#%%
+mean_volume = df_2020.groupby('PERMNO')['VOL'].mean()
+    
+    
+
+    
+
     
 
 
-
-    
     
 
 
--- functions.py
+++ functions.py
@@ -1,23 +1,26 @@
 import pandas as pd 
 import numpy as np
 
-
-
 def RV(df, typeVOL = None): 
     #First calculate daily Volatility
+    
+    
     df['dVOL'] = np.log(df['ASKHI']) - np.log(df['BIDLO'])
     if(typeVOL != None):
         if(len(typeVOL)>0): 
             for typ in typeVOL:
-                df[str(typ)+'VOL'] = (df['dVOL'].rolling(typ).sum())/typ    
+                df[str(typ)+'VOL'] = df.groupby('PERMNO')['dVOL'].rolling(typ).mean().reset_index(0,drop=True)
         else:
-            df[str(typeVOL)+'VOL'] = (df['dVOL'].rolling(typeVOL).sum())/typeVOL
+            df[str(typeVOL)+'VOL'] = df.groupby('PERMNO')['dVOL'].rolling(typVOL).mean().reset_index(0,drop=True)
     
     return(df)
 
 
+
 def sorted_enumerate(seq):
     return [i for (v, i) in sorted((v, i) for (i, v) in enumerate(seq))]
+
+
 
 
 
